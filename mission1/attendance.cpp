eba692e00707da1af2f03a07591be8bc0f879430
s5mug <lsj960427@gmail.com>
2025-08-21 10:19:56 +0900

    mission1 done

--- mission1/attendance.cpp
+++ mission1/attendance.cpp
@@ -8,133 +8,193 @@
 using namespace std;
 
 struct WeekDayData {
-	string nameStr;
+	string playerName;
 	string dateStr;
 };
 
-map<string, int> id1;
-int id_cnt = 0;
+enum DATE_ID{
+	MONDAY = 0,
+	TUSEDAY = 1,
+	WEDNESDAY = 2,
+	THURSDAY = 3,
+	FRIDAY = 4,
+	SATURDAY = 5,
+	SUNDAY = 6,
+	MAX_NUM_DATE = 7
+};
 
-//dat[사용자ID][요일]
-int attendance_count_by_date[100][100];
-int points[100];
-int grade[100];
-string names[100];
+enum Grade_e {
+	GRADE_NORMAL = 0,
+	GRADE_GOLD = 1,
+	GRADE_SILVER = 2
+};
 
-int attendance_wendsday[100];
-int attendance_weekend[100];
-
-void input2(const WeekDayData &weekdayData) {
-	//ID 부여
-	if (id1.count(weekdayData.nameStr) == 0) {
-		id1.insert({ weekdayData.nameStr, ++id_cnt });
-
-		if (weekdayData.nameStr == "Daisy") {
-			int debug = 1;
-		}
-
-		names[id_cnt] = weekdayData.nameStr;
-	}
-	int id2 = id1[weekdayData.nameStr];
-
-	//디버깅용
-	if (weekdayData.nameStr == "Daisy") {
-		int debug = 1;
-	}
-
-
-	int attendance_point = 0;
-	int index = 0;
-	if (weekdayData.dateStr == "monday") {
-		index = 0;
-		attendance_point++;
-	}
-	if (weekdayData.dateStr == "tuesday") {
-		index = 1;
-		attendance_point++;
-	}
-	if (weekdayData.dateStr == "wednesday") {
-		index = 2;
-		attendance_point += 3;
-		attendance_wendsday[id2] += 1;
-	}
-	if (weekdayData.dateStr == "thursday") {
-		index = 3;
-		attendance_point++;
-	}
-	if (weekdayData.dateStr == "friday") {
-		index = 4;
-		attendance_point++;
-	}
-	if (weekdayData.dateStr == "saturday") {
-		index = 5;
-		attendance_point += 2;
-		attendance_weekend[id2] += 1;
-	}
-	if (weekdayData.dateStr == "sunday") {
-		index = 6;
-		attendance_point += 2;
-		attendance_weekend[id2] += 1;
-	}
-
-	//사용자ID별 요일 데이터에 1씩 증가
-	attendance_count_by_date[id2][index] += 1;
-	points[id2] += attendance_point;
+bool IsValidDate(DATE_ID date_id) {
+	return date_id != MAX_NUM_DATE;
 }
 
-void input() {
-	ifstream fin{ "attendance_weekday_500.txt" }; //500개 데이터 입력
-	for (int i = 0; i < 500; i++) {
+map<string, int> player_name_to_Id_map;
+int max_player_id = 0;
+const int MAX_NUM_PLAYER_ID = 100;
+
+//dat[사용자ID][요일]
+int attendance_count_by_date[MAX_NUM_PLAYER_ID][MAX_NUM_DATE];
+int player_point[MAX_NUM_PLAYER_ID];
+Grade_e grade[MAX_NUM_PLAYER_ID];
+string playerNames[MAX_NUM_PLAYER_ID];
+
+
+int getPlayerIdFromName(const string& playerName) {
+	if (player_name_to_Id_map.count(playerName) == 0) {
+		player_name_to_Id_map.insert({ playerName, ++max_player_id });
+		playerNames[max_player_id] = playerName;
+	}
+	return player_name_to_Id_map[playerName];
+}
+
+DATE_ID getDateIndexFromDateStr(const string& dateStr) {
+	if (dateStr == "monday") {
+		return MONDAY;
+	}
+	if (dateStr == "tuesday") {
+		return TUSEDAY;
+	}
+	if (dateStr == "wednesday") {
+		return WEDNESDAY;
+	}
+	if (dateStr == "thursday") {
+		return THURSDAY;
+	}
+	if (dateStr == "friday") {
+		return FRIDAY;
+	}
+	if (dateStr == "saturday") {
+		return SATURDAY;
+	}
+	if (dateStr == "sunday") {
+		return SUNDAY;
+	}
+	return MAX_NUM_DATE;
+}
+
+int getBasePointByDate(DATE_ID date_Id) {
+	const int BASE_POINT_WEDNESDAY = 3;
+	const int BASE_POINT_WEEKEND = 2;
+	const int BASE_POINT_DEFAULT = 1;
+
+	switch (date_Id) {
+	case WEDNESDAY:
+		return BASE_POINT_WEDNESDAY;
+	case SATURDAY:
+	case SUNDAY:
+		return  BASE_POINT_WEEKEND;
+	default:
+		return BASE_POINT_DEFAULT;
+	}
+}
+
+void updatePlayer(int player_Id, DATE_ID date_Id) {
+	if (false == IsValidDate(date_Id)) return;
+
+	player_point[player_Id] += getBasePointByDate(date_Id);
+	attendance_count_by_date[player_Id][date_Id] += 1;
+}
+
+
+int getBonusPointByWendsDay(int attendance_count_wednesday) {
+	const int WEDNESDAY_BONUS__POINT_THRESHOLD = 10;
+	const int WEDNESDAY_BONUS_PONINT = 10;
+	if (attendance_count_wednesday >= WEDNESDAY_BONUS__POINT_THRESHOLD) 
+		return WEDNESDAY_BONUS_PONINT;
+	return 0;
+}
+
+int getBonusPointByWeekend(int attendance_count_weekend) {
+	const int WEEKEND_BONUS_POINT_THRESHOLD = 10;
+	const int WEEKEND_BONUS_PONINT = 10;
+
+	if (attendance_count_weekend >= WEEKEND_BONUS_POINT_THRESHOLD)
+		return WEEKEND_BONUS_PONINT;
+	return 0;
+}
+
+Grade_e getGradeFromPoint(int point) {
+	const int GOLD_GRADE_THRESHOLD = 50;
+	const int SILVER_GRADE_THRESHOLD = 30;
+
+	if (point >= GOLD_GRADE_THRESHOLD) return GRADE_GOLD;
+	if (point >= SILVER_GRADE_THRESHOLD) return GRADE_SILVER;
+	return GRADE_NORMAL;
+}
+
+
+void summarizePlayers() {
+	for (int player_id = 1; player_id <= max_player_id; player_id++) {
+		player_point[player_id] += getBonusPointByWendsDay(attendance_count_by_date[player_id][WEDNESDAY]);
+
+		int attendance_count_weekend = attendance_count_by_date[player_id][SATURDAY] + attendance_count_by_date[player_id][SUNDAY];
+		player_point[player_id] += getBonusPointByWeekend(attendance_count_weekend);
+
+		grade[player_id] = getGradeFromPoint(player_point[player_id]);
+	}
+}
+
+const char* getGradeStr(int grade) {
+	if (grade == GRADE_GOLD) return "GOLD";
+	if (grade == GRADE_SILVER) return "SILVER";
+	return "NORMAL";
+}
+
+const char* getGradeStrFromPlayerId(int player_id) {
+	return getGradeStr(grade[player_id]);
+}
+
+const bool IsRemovedPlayer(Grade_e grade, int (&player_attendance_count_by_date)[MAX_NUM_DATE]) {
+	if (grade == GRADE_GOLD || grade == GRADE_SILVER) return false;
+	if (player_attendance_count_by_date[WEDNESDAY] > 0) return false;
+	if (player_attendance_count_by_date[SATURDAY] > 0) return false;
+	if (player_attendance_count_by_date[SUNDAY] > 0) return false;
+	return true;
+}
+
+vector<int> getRemovedPlayerId(){
+	vector<int> result;
+	for (int player_id = 1; player_id <= max_player_id; player_id++) {
+		if (IsRemovedPlayer(grade[player_id], attendance_count_by_date[player_id])) {
+			result.push_back(player_id);
+		}
+	}
+	return result;
+}
+
+void attendanceManageWeekdayDataFile(const char* weekdayDataFileName, int dataNum) {
+
+	ifstream fin{ weekdayDataFileName };
+	for (int i = 0; i < dataNum; i++) {
 		WeekDayData weekdayData;
-		fin >> weekdayData.nameStr >> weekdayData.dateStr;
-		input2(weekdayData);
+		fin >> weekdayData.playerName >> weekdayData.dateStr;
+		updatePlayer(getPlayerIdFromName(weekdayData.playerName), getDateIndexFromDateStr(weekdayData.dateStr));
 	}
 
-	for (int i = 1; i <= id_cnt; i++) {
-		if (attendance_count_by_date[i][2] > 9) {
-			points[i] += 10;
-		}
+	summarizePlayers();
 
-		if (attendance_count_by_date[i][5] + attendance_count_by_date[i][6] > 9) {
-			points[i] += 10;
-		}
-
-		if (points[i] >= 50) {
-			grade[i] = 1;
-		}
-		else if (points[i] >= 30) {
-			grade[i] = 2;
-		}
-		else {
-			grade[i] = 0;
-		}
-
-		cout << "NAME : " << names[i] << ", ";
-		cout << "POINT : " << points[i] << ", ";
-		cout << "GRADE : ";
-
-		if (grade[i] == 1) {
-			cout << "GOLD" << "\n";
-		}
-		else if (grade[i] == 2) {
-			cout << "SILVER" << "\n";
-		}
-		else {
-			cout << "NORMAL" << "\n";
-		}
+	for (int player_id = 1; player_id <= max_player_id; player_id++) {
+		cout << "NAME : " << playerNames[player_id] << ", ";
+		cout << "POINT : " << player_point[player_id] << ", ";
+		cout << "GRADE : " << getGradeStrFromPlayerId(player_id) << "\n";
 	}
 
 	std::cout << "\n";
 	std::cout << "Removed player\n";
 	std::cout << "==============\n";
-	for (int i = 1; i <= id_cnt; i++) {
-
-		if (grade[i] != 1 && grade[i] != 2 && attendance_wendsday[i] == 0 && attendance_weekend[i] == 0) {
-			std::cout << names[i] << "\n";
-		}
+	vector<int> removedPlayerId = getRemovedPlayerId();
+	for (int player_Id : removedPlayerId) {
+		std::cout << playerNames[player_Id] << "\n";
 	}
 }
 
 int main() {
-	input();
+	const char* weekdayDataFileName = "attendance_weekday_500.txt";
+	const int WEEKDAY_DATA_NUM = 500;
+	attendanceManageWeekdayDataFile(weekdayDataFileName, WEEKDAY_DATA_NUM);
 }
